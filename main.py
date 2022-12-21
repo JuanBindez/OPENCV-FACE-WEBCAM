9f8b3f8e6804d07c91af3f251891a4dbf31fbcb6
Juan Carlos Bindez <juanbindez780@gmail.com>
2022-12-21 14:40:00 -0300

    2112221439

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-xml_haarcascade = "haarcascade_frontalface_alt2.xml"
+xml_haarcascade = "haarcascade_frontalface_alt2.xml"                         # Arquivo classificador
 
-face_classifier = cv2.CascadeClassifier(xml_haarcascade)                     # Carrega o clicificador.
+face_classifier = cv2.CascadeClassifier(xml_haarcascade)                     # Carrega o clissificador.
 
 
 # Inicia camera:
 capture = cv2.VideoCapture(0)
 
+# Define o tamanho da imagem:
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 340)
-
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 350)
+
 
 while not cv2.waitKey(20) & 0xFF == ord("q"):
     ret, frame_color = capture.read()
